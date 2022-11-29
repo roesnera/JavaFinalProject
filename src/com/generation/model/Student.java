ef3f0e5867ac12bda39d9825f6d87fa44b38589c
yummygoods <melissa@yummygoods.com>
2022-11-29 11:48:34 -0500

    i added your solutions and commented them but you can see my original answers still

--- src/com/generation/model/Student.java
+++ src/com/generation/model/Student.java
@@ -6,47 +6,79 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class Student
     extends Person
-    implements Evaluation
-{
+    implements Evaluation {
     private double average;
 
     private final List<Course> courses = new ArrayList<>();
 
     private final Map<String, Course> approvedCourses = new HashMap<>();
 
-    public Student( String id, String name, String email, Date birthDate )
-    {
-        super( id, name, email, birthDate );
+    public Student(String id, String name, String email, Date birthDate) {
+        super(id, name, email, birthDate);
     }
 
-    public void enrollToCourse( Course course )
-    {
+    public void enrollToCourse(Course course) {
+
         //TODO implement this method
+//well this was a lot simpler than i was making it 🙄
+   courses.add(course);
+
     }
 
-    public void registerApprovedCourse( Course course )
-    {
-        approvedCourses.put( course.getCode(), course );
+    public void registerApprovedCourse(Course course) {
+        approvedCourses.put(course.getCode(), course);
     }
 
 
-    public boolean isAttendingCourse( String courseCode )
-    {
+    public boolean isAttendingCourse(String courseCode) {
+
         //TODO implement this method
+        //use getCode method
+        //loop over the list of courses to see if any match
+        //if course code passed in as arg, is included in the course list
+        // return true
+        //if not  return false;
+        int i = 0;
+
+        while (i < courses.size()) {
+
+
+            if (i == courses.indexOf(courseCode)) {
+                return true;
+            }
+
+        }
         return false;
+        // omg did i do it? not getting the red squiggles! would this work?
+
+
+
+        //from adam's example:
+        //i really need to get more comfortable with loops because i completely forgot about this type
+      for(Course c : courses){
+        if(c.getCode().equals(courseCode)){
+            return true;
+        }
+        return false;
+
+
+
     }
+
 
     @Override
-    public double getAverage()
-    {
+    public double getAverage(){
+
         return average;
     }
 
     @Override
-    public String toString()
-    {
-        return "Student {" + super.toString() + "}";
+    public String toString() {
+
+        return "Student {" + super.toString() + "}"
     }
+
 }
