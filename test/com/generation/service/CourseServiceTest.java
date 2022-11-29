f39b925dfaa299facea52433d1b26ecc805afcff
mitch <mitch01nguyen@gmail.com>
2022-11-29 12:04:26 -0600

    adds one test per file

--- test/com/generation/service/CourseServiceTest.java
+++ test/com/generation/service/CourseServiceTest.java
@@ -1,5 +1,7 @@
 package com.generation.service;
 
+import com.generation.model.Course;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,11 +9,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class CourseServiceTest {
     //import CourseService object
     public CourseService courseService;
+    //implement this constructor before each test
+    @BeforeEach
+    void setUp(){
+        courseService = new CourseService();
+    }
     @Test
     void getCourse() {
+        //assigning a var to getCourse's answer and converting it as a string
+        String code = String.valueOf(courseService.getCourse("INTRO-WEB-1"));
+        //assigning the answer string as a var
+        String answer = "Course{code='INTRO-WEB-1', name='Introduction to Web Applications', credits=9, module=Module{name='Web Development Fundamentals'}}";
+        assertEquals(answer,code);
+    }
+    @Test
+    void getCourseNull() {
+        //assigning a var to getCourse's answer and converting it as a string
+        String code = String.valueOf(courseService.getCourse("INTRO-W3B-2"));
+        //assigning the expected answer string as a var
+        String answer = "null";
+        //null is in a string because we made the valueOf null into a string within code assignment
+        assertEquals(answer,code);
     }
 
-    @Test
-    void showEnrolledStudents() {
-    }
 }
--- test/com/generation/service/StudentServiceTest.java
+++ test/com/generation/service/StudentServiceTest.java
@@ -1,22 +1,28 @@
 package com.generation.service;
 
 import com.generation.model.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentServiceTest {
-    //import student object
+    //import student and studentService object
     public Student student;
-    Student mitch = new Student("1", "mitch", "email", null);
-    Student vector = new Student("2","vector", "email2", null);
+    public StudentService studentService;
 
+    //execute before each test
+    @BeforeEach
+    void setUp(){
+        studentService = new StudentService();
+        //initialize students
+        Student mitch = new Student("1", "mitch", "email", null);
+        Student vector = new Student("2","vector", "email2", null);
+    }
     @Test
     void findStudent() {
+        Student temp = studentService.findStudent("2");
+        assertEquals(null, temp );
 
     }
 
-    @Test
-    void StudentServiceTest() {
-
-    }
 }
