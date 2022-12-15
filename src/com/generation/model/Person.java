08cf09a6ecee4982abcf678d99e1cac55d18c0b8
taseffa <113387290+taseffa@users.noreply.github.com>
2022-12-14 21:00:58 -0500

    Java final project assignment

--- src/com/generation/model/Person.java
+++ src/com/generation/model/Person.java
@@ -43,6 +43,6 @@ abstract public class Person
     @Override
     public String toString()
     {
-        return id + '\'' + ", name='" + name + '\'' + ", email='" + email + '\'' + ", birthDate=" + birthDate;
+        return "'"+ id + '\'' + ", name='" + name + '\'' + ", email='" + email + '\'' + ", birthDate=" + birthDate;
     }
 }
